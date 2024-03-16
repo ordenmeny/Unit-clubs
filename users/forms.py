a778966a9caafe6e1b54ed506c7ffa0eea0aec74
Dmitriy Alexandrov <hogvarts06@mail.ru>
2024-03-16 15:35:18 +0500

    Верстка страницы регистрации

--- users/forms.py
+++ users/forms.py
@@ -5,20 +5,33 @@ from django import forms
 
 
 class UserForm(UserCreationForm):
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput())
+    username = UsernameField(
+        widget=forms.TextInput(attrs={"autofocus": True, 'class': 'form-control block-opacity input-height',
+                                      'placeholder': 'Имя пользователя'}))
+    fio = forms.CharField(label='ФИО',
+                          widget=forms.TextInput(attrs={'class': 'form-control block-opacity input-height', 'placeholder': 'ФИО'}))
+    tg = forms.CharField(label='Телеграм',
+                         widget=forms.TextInput(attrs={'class': 'form-control block-opacity input-height', 'placeholder': 'Телеграм'}))
+    desc = forms.CharField(label='Описание',
+                           widget=forms.Textarea(attrs={'class': 'form-control block-opacity input-height', 'placeholder': 'Описание'}))
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control block-opacity input-height', 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(label='Повторите пароль',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control block-opacity input-height', 'placeholder': 'Повторите пароль'}))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'fio', 'tg', 'desc', 'password1', 'password2',  'image']
+        fields = ['username', 'fio', 'tg', 'desc', 'password1', 'password2', 'image']
 
 
 class LoginForm(AuthenticationForm):
     username = UsernameField(
-        widget=forms.TextInput(attrs={"autofocus": True, 'class': 'form-control block-opacity input-height', 'placeholder': 'Имя пользователя'}))
+        widget=forms.TextInput(attrs={"autofocus": True, 'class': 'form-control block-opacity input-height',
+                                      'placeholder': 'Имя пользователя'}))
     password = forms.CharField(
         strip=False,
         widget=forms.PasswordInput(
-            attrs={"autocomplete": "current-password", 'class': 'form-control block-opacity input-height', 'placeholder': 'Пароль'}
+            attrs={"autocomplete": "current-password", 'class': 'form-control block-opacity input-height',
+                   'placeholder': 'Пароль'}
         ),
     )
