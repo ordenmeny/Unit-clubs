4687908ef86b351d1bee62ff945daff0328dac1e
Dmitriy Alexandrov <hogvarts06@mail.ru>
2024-03-31 00:45:30 +0500

    changes

--- project_clubs/settings.py
+++ project_clubs/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_$@5o+p_l^cs$e-#crd+p9l)v4086x@duc2zs$du_!fk9mvq(4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ordenmeny-unit-clubs-92cf.twc1.net']
 
 # Application definition
 
