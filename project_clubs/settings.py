d04098d1db0c57bf0cd307b6a034c33068335861
Dmitriy Alexandrov <hogvarts06@mail.ru>
2024-03-31 15:37:14 +0500

    commit

--- project_clubs/settings.py
+++ project_clubs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_$@5o+p_l^cs$e-#crd+p9l)v4086x@duc2zs$du_!fk9mvq(4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
