fc0c75aa7f003241781ff811db4e31ffc8eda7fe
Dmitriy Alexandrov <hogvarts06@mail.ru>
2024-04-01 21:33:36 +0500

    ommit

--- project_clubs/settings.py
+++ project_clubs/settings.py
@@ -234,6 +234,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR, 'static',
 # ]
+# commmit
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
